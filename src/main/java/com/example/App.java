50262f112d5e21cb6e8aaaacfa8d6a412b2a8bb3
priyanka-ochaney13 <priyanka.ochaney@gmail.com>
2025-12-28 19:33:46 +0530

    onetoone

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,14 +4,36 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.Transaction;
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
+        Laptop l1 = new Laptop();
+        l1.setLid(1);
+        l1.setBrand("Dell");
+        l1.setModel("Inspiron");
+    
+        Alien a1 = new Alien();
+        a1.setAid(1);
+        a1.setAname("Priyanka");
+        a1.setTech("Java");
+        a1.setLaptop(l1);
         
+        SessionFactory sf = new Configuration()
+                                .configure()
+                                .addAnnotatedClass(Alien.class)
+                                .addAnnotatedClass(Laptop.class)
+                                .buildSessionFactory();
+
+        Session s = sf.openSession();
+        Transaction tx = s.beginTransaction();
+        s.persist(l1);
+        s.persist(a1);
+        tx.commit();
+        Alien a2 = s.find(Alien.class, 1);
+        System.out.println(a2);
+        s.close();
+        sf.close();
     }
 }
--- src/main/java/com/example/Laptop.java
+++ src/main/java/com/example/Laptop.java
@@ -1,5 +1,35 @@
 package com.example;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Laptop {
-    
+    @Id
+    private int lid;
+    private String brand;
+    private String model;
+
+    public int getLid() {
+        return lid;
+    }
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    @Override
+    public String toString() {
+        return "Laptop [lid=" + lid + ", brand=" + brand + ", model=" + model + "]";
+    }
 }
