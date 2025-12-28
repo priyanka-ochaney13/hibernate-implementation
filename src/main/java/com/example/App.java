c060ec4a369ba855e44d4e5eb179eb3ff27c1ead
priyanka-ochaney13 <priyanka.ochaney@gmail.com>
2025-12-28 18:44:56 +0530

    first

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
